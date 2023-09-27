8cd140c6d848e023bd23095d93ea0e60d7a2b88b
LucasIrineuValle <lucas.v02@aluno.ifsc.edu.br>
2023-09-27 17:10:33 -0300

    atualizando

--- src/lulewill/SistemaMain.java
+++ src/lulewill/SistemaMain.java
@@ -1,11 +1,40 @@
 package lulewill;
 
+import java.util.Scanner;
+
 public class SistemaMain {
 
 	public static void main(String[] args) {
-		System.out.println("Teste para o Lucas e Lezio");
-		System.out.println("Git é magico");
-
+		
+		Sacanner leitura = new Scanner (System.in);
+		
+		int opcaoSelecionada = 1;
+		
+		while(opcaoSelecionada != 0) {
+		
+		System.out.println("- MENU EXEMPLO -");
+		System.out.println();
+		System.out.println("1 CADASTRAR VEICULO");
+		System.out.println("2 PROUCURAR VEICULO");
+		System.out.println("3 ALTERAR PEDIDO");
+		System.out.println("0 SAIR");
+		System.out.println();
+		
+		opcaoSelecionada = leitura.nextInt();
+		
+		switch (opcaoSelecionada) {
+		case 0: {
+			
+			break;
+			
+		}
+		case 1: {
+			
+			
+		}
+		
+		
+		}
 	}
 
 }
